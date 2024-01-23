8b59400a3b99545827e30bbedf1eae1f82907393
Mikison <95968188+Mikison@users.noreply.github.com>
2024-01-23 20:08:49 +0100

    klub more info

--- angular-frontend/src/app/components/kluby/klub-info/klub-info.component.ts
+++ angular-frontend/src/app/components/kluby/klub-info/klub-info.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-klub-info',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './klub-info.component.html',
-  styleUrl: './klub-info.component.css'
+  styleUrl: './klub-info.component.css',
 })
-export class KlubInfoComponent {
-
-}
+export class KlubInfoComponent {}
