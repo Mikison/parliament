873c19ac50ccfd45823a716299cbef564c98276e
Mikison <95968188+Mikison@users.noreply.github.com>
2024-01-24 17:10:24 +0100

    pozdro

--- angular-frontend/src/app/components/kluby/kluby.component.ts
+++ angular-frontend/src/app/components/kluby/kluby.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-kluby',
   standalone: true,
-  imports: [ButtonComponent, MatIconModule],
+  imports: [ButtonComponent, MatIconModule, RouterModule],
   templateUrl: './kluby.component.html',
   styleUrl: './kluby.component.css',
 })
